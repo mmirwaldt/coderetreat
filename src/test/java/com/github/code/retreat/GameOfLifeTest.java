678117402f71bd59d4ee3df88fb36b4d2d30450e
mmirwaldt <michael.mirwaldt@gmail.com>
2020-11-07 10:55:34 +0100

    first test

---
+++ src/test/java/com/github/code/retreat/GameOfLifeTest.java
@@ -0,0 +1,12 @@
+package com.github.code.retreat;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class GameOfLifeTest {
+    @Test
+    void test() {
+        assertEquals(true, 1==1);
+    }
+}
