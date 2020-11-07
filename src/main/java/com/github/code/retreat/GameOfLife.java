c42d840c9db80f8e37c5879c6e62f717ecf00e54
mallam <mallamdevgit@gmail.com>
2020-11-07 12:03:12 +0100

    GameOfLife in coderetreat

--- src/main/java/com/github/code/retreat/GameOfLife.java
+++ src/main/java/com/github/code/retreat/GameOfLife.java
@@ -1,4 +1,7 @@
 package com.github.code.retreat;
 
 public class GameOfLife {
+    public boolean willSurvive(int lifeNeighbors) {
+        return lifeNeighbors == 3;
+    }
 }
--- src/test/java/com/github/code/retreat/GameOfLifeTest.java
+++ src/test/java/com/github/code/retreat/GameOfLifeTest.java
@@ -2,11 +2,35 @@ package com.github.code.retreat;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class GameOfLifeTest {
+    GameOfLife gameOfLife = new GameOfLife();
+
     @Test
-    void test() {
-        assertEquals(true, 1==1);
+    void testWillSurviveWhenThreeNeighborsIsLife() {
+        assertTrue(gameOfLife.willSurvive(3));
     }
+
+    @Test
+    void testWillSurviveWhenZeroNeighborsIsLife() {
+        assertFalse(gameOfLife.willSurvive(0));
+    }
+
+
+    @Test
+    void testWillSurviveWhenFiveNeighborsIsLife() {
+        assertFalse(gameOfLife.willSurvive(5));
+    }
+
+    @Test
+    void testWillSurviveWhenTwoNeighborsIsLife() {
+        assertFalse(gameOfLife.willSurvive(2));
+    }
+
+    @Test
+    void testWillSurviveWhenMinusOneNeighborsIsLife() {
+        assertThrows(IllegalArgumentException.class,()->gameOfLife.willSurvive(-1));
+    }
+
 }
